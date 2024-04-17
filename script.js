0742fb8f92c306a47ae807b1b8df1b9dafa12016
Yashraj_K <yashrajkamble7727@gamil.com>
2024-04-17 13:50:01 +0530

    js done

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hasdsakdfj")
+let selectField = document.getElementById("selectField");
+let selectText = document.getElementById("selectText");
+let options = document.getElementsByClassName("options");
+let list = document.getElementById("list");
+let arrowIcon = document.getElementById("arrowIcon")
+
+selectField.onclick = function () {
+    list.classList.toggle("hide");
+    arrowIcon.classList.toggle("rotate");
+}
+
+for (option of options) {
+    option.onclick = function () {
+        selectText.innerHTML = this.textContent;
+        list.classList.toggle("hide");
+        arrowIcon.classList.toggle("rotate");
+    }
+}
